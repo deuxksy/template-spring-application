353a7e02d6af19c8dab149e4a03f60bc2883c268
SeokYoung Kim <deuxksy@gmail.com>
2019-10-10 02:08:32 +0900

    Initial commit

---
+++ src/main/java/com/zzizily/Main.java
@@ -0,0 +1,9 @@
+package com.zzizily;
+
+import com.zzizily.common.GradleProperties;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(GradleProperties.getProperty("lombok"));
+  }
+}
